9f6417e2daa20e911f68382809bcb03cb61faad5
jonathan <jonathan@kitomba.com>
2020-01-30 12:51:16 +1300

    Removing the neuronia parasite to its own Repo, RubbishContainer can be used?!.

--- src/test/java/com/rubbishman/rubbishRedux/internal/neuronia/state/card/pathway/Movement.java
+++
@@ -1,8 +0,0 @@
-package com.rubbishman.rubbishRedux.internal.neuronia.state.card.pathway;
-
-public enum Movement {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
-}
--- src/test/java/com/rubbishman/rubbishRedux/internal/neuronia/state/card/skill/DoublePickup.java
+++
@@ -1,4 +0,0 @@
-package com.rubbishman.rubbishRedux.internal.neuronia.state.card.skill;
-
-public class DoublePickup {
-}
